df4730a3bfef2949d174f22acf0655f222f8246f
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-07-11 15:05:31 -0400

    Updated to be async

--- cjs/index.js
+++ cjs/index.js
@@ -1,5 +1,5 @@
 const Compiler = require('htl-compiler');
 
 console.log(
-  new Compiler("<div>${message}</div>", { message: "Hello, World!" }).compile()
+  new Compiler("<div>${message}</div>", { message: "Hello, World!" }).compileSync()
 );
