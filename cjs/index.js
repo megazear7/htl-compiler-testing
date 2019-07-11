c75122a168cd4370230d40f2bdc05a4054b0b5b2
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-07-11 10:47:13 -0400

    Created working cjs example. Stubbed out esm and umd examples.

---
+++ cjs/index.js
@@ -0,0 +1,5 @@
+const Compiler = require('htl-compiler');
+
+console.log(
+  new Compiler("<div>${message}</div>", { message: "Hello, World!" }).compile()
+);
---
+++ umd/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use('/static', express.static(path.join(__dirname, './public')));
+app.use('/htl-compiler', express.static(path.join(__dirname, '../node_modules/htl-compiler')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
